612cb6ad7743cd2d06fb0b3ba7966bc2731db600
Brian Lin <brian@spanscale.com>
2025-09-06 16:41:04 +0800

    feat: add font-helvetica-neue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-2xl font-bold'>Hello World!</h1>
+    <div className='flex flex-col items-center justify-center h-screen bg-black'>
+      <h1 className="font-helvetica-neue font-bold text-[300px] text-[#FFD922]">
+        Studio
+      </h1>
     </div>
   )
 }
